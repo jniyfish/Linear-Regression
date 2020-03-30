f79fffbd1a0187110750da74267c324f51ac7148
jniyfish <39803423+jniyfish@users.noreply.github.com>
2020-03-30 12:56:37 +0800

    Add files via upload

--- function.py
+++ function.py
@@ -2,10 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import sample
 
+def NormalEquation(nA,testY,shape):
+	nA = nA.reshape(shape,2)
+	testY = testY.reshape(shape,1)
+	theta = np.dot(nA.T,nA)
+	if np.linalg.det(theta) == 0: #singular
+		theta = np.dot(np.linalg.pinv(theta),np.dot(nA.T,testY)) 
+	elif np.linalg.det(theta) !=0 : #nonsingular
+		theta = np.dot(np.linalg.inv(theta),np.dot(nA.T,testY)) 
+	return theta
+
+
 def LeaveOneOut(x,y):
 	nA = np.array([])
 	errorSum = 0
-	for i in range(1,20):
+	for i in range(0,20):
 		testX = np.array([])
 		trainX = np.array([])
 		testY = np.array([])
@@ -21,15 +32,72 @@ def LeaveOneOut(x,y):
 				nA = np.append(nA,[x[i],1])
 
 		trainYe = trainY
-		nA = nA.reshape(19,2)
-		trainY = trainY.reshape(19,1)
-		theta = np.dot(nA.T,nA)
-		if np.linalg.det(theta) == 0:
-			theta = np.dot(np.linalg.pinv(theta),np.dot(nA.T,trainY)) 
-		elif np.linalg.det(theta) !=0 :
-			theta = np.dot(np.linalg.inv(theta),np.dot(nA.T,trainY)) 
+		theta = NormalEquation(nA,trainY,19)
+			
 		testError = testY - (theta[0]*testX + theta[1])
 		testError = np.dot(testError.T,testError)/1
 		errorSum = errorSum + testError
 	errorSum = errorSum/20
-	print(errorSum)
+	print("LOO error:	",errorSum)
+
+
+def LinearRegression(x,y):
+
+	testX=np.array([])
+	testY =np.array([])
+	trainX=np.array([])
+	trainY=np.array([])
+	nA = np.array([]) # for normal equtaion
+
+	for i in range(0,20): #spilt data 
+		if i%4==0 :
+			testX = np.append(testX,x[i])
+			testY = np.append(testY,y[i])
+		elif i%4!=0 :
+			trainX = np.append(trainX,x[i])
+			nA = np.append(nA,[x[i],1])
+			trainY = np.append(trainY,y[i])
+
+	trainYe = trainY
+	theta = NormalEquation(nA,trainY,15)
+
+	trainError = trainYe - (theta[0]*trainX + theta[1])
+	trainError = np.dot(trainError.T,trainError)/15#error = (y-y')^2 summation
+	print("train Error:	",trainError)
+
+	testError = testY - (theta[0]*testX + theta[1]) # y^ = y-y'
+	testError = np.dot(testError.T,testError) /5        #error = (y-y')^2 summation
+	print("test Error:	",testError)
+	plt.scatter(testX,testY,s=30,c='blue',marker='o',alpha=0.5,label='test(5)')
+	plt.scatter(trainX,trainY,s=30,c='green',marker='x',alpha=0.5,label='train(15)')
+	return theta
+
+def Five_Fold(x,y):
+
+#	nA = np.array([])
+	errorSum = 0
+	for i in range(0,5):
+		testX = np.array([])
+		trainX = np.array([])
+		testY = np.array([])
+		trainY = np.array([])
+		nA = np.array([])
+		for j in range(0,20):
+			if (4*i)+4>j and j >=(4*i) :
+				testX = np.append(testX,x[i])
+				testY = np.append(testY,y[i])
+			else :
+				trainX = np.append(trainX,x[i])
+				trainY = np.append(trainY,y[i])
+				nA = np.append(nA,[x[i],1])
+
+		theta = NormalEquation(nA,trainY,16)
+
+		testError = testY - (theta[0]*testX + theta[1])
+		testError = np.dot(testError.T,testError)/4  #each time has 4 MSE
+		print(testError)
+		errorSum = errorSum + testError
+
+	errorSum = errorSum/5    #five fold
+	print("5_Fold error:	",errorSum)
+
--- hw1.py
+++ hw1.py
@@ -11,41 +11,12 @@ x = np.linspace(3,-3,20)
 y=2*x+noise
 
 
-testX=np.array([])
-testY =np.array([])
-trainX=np.array([])
-trainY=np.array([])
-nA = np.array([]) # for normal equtaion
 
-for i in range(0,20): #spilt data 
-	if i%4==0 :
-		testX = np.append(testX,x[i])
-		testY = np.append(testY,y[i])
-	elif i%4!=0 :
-		trainX = np.append(trainX,x[i])
-		nA = np.append(nA,[x[i],1])
-		trainY = np.append(trainY,y[i])
-
-trainYe = trainY
-nA = nA.reshape(15,2)                #use training data to slove normal equation
-trainY = trainY.reshape(15,1)
-theta = np.dot(nA.T,nA)
-theta = np.dot(np.linalg.inv(theta),np.dot(nA.T,trainY)) 
-
-
-trainError = trainYe - (theta[0]*trainX + theta[1])
-trainError = np.dot(trainError.T,trainError)/15#error = (y-y')^2 summation
-print("train Error:	",trainError)
-
-testError = testY - (theta[0]*testX + theta[1]) # y^ = y-y'
-testError = np.dot(testError.T,testError) /5        #error = (y-y')^2 summation
-print("test Error:	",testError)
+theta = fun.LinearRegression(x,y)
+fun.LeaveOneOut(x,y)
+fun.Five_Fold(x,y)
 myLine = theta[0]*x + theta[1]
 
-fun.LeaveOneOut(x,y)
-
-plt.scatter(testX,testY,s=30,c='blue',marker='o',alpha=0.5,label='test(5)')
-plt.scatter(trainX,trainY,s=30,c='green',marker='x',alpha=0.5,label='train(15)')
 
 plt.xlabel('x軸',fontsize=8)
 plt.ylabel('y軸',fontsize=8,rotation=0)
