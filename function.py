7d71a3b9c375b02e6f94cc4a6843025a982afe6c
jniyfish <39803423+jniyfish@users.noreply.github.com>
2020-04-01 16:50:43 +0800

    Add files via upload

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import math
 from random import sample
 
-def NormalEquation(nA,testY,shape_m,shape_n):  #X:mxn matrix
+def NormalEquation(nA,testY,shape_m,shape_n):  #nA = X:mxn matrix
 	nA = nA.reshape(shape_m,shape_n)
 	testY = testY.reshape(shape_m,1)
 	theta = np.dot(nA.T,nA)
@@ -17,7 +17,7 @@ def NormalEquation(nA,testY,shape_m,shape_n):  #X:mxn matrix
 
 def LeaveOneOut(x,y,degree,dataSize):
 	trainSize = dataSize - 1
-	testSize = 1
+	testSize = 1   #only one test
 	errorSum = 0
 	for i in range(0,dataSize):
 		testX = np.array([])
@@ -46,8 +46,8 @@ def LeaveOneOut(x,y,degree,dataSize):
 
 
 def Five_Fold(x,y,degree,dataSize):
-	trainSize = dataSize*4//5
-	testSize = dataSize//5
+	trainSize = dataSize*4//5  #each time use 4/5 for train 
+	testSize = dataSize//5     # 1/5 for test
 	errorSum = 0
 	for i in range(0,5):
 		testX = np.array([])
@@ -89,7 +89,7 @@ def Reression(nA,plotX,testX,testY,trainX,trainY,degree,trainSize,testSize):
 		j = j - 1
 	fitY = fitY + theta[degree]
 	fitTestY = fitTestY + theta[degree]
-	trainError = trainY - fitY
+	trainError = trainY - fitY  
 	trainError =  np.dot(trainError.T,trainError)/trainSize
 	testError = testY - fitTestY
 	testError = np.dot(testError.T,testError)/testSize
@@ -107,26 +107,27 @@ def linearRegression(x,y,degree,dataSize):
 	plotX = np.array([])
 	for i in range(0,dataSize): #spilt data 
 		row = np.array([])
-		if i%4==2 :
+		if i%4==2 :  #1/4 for test
 			testX = np.append(testX,x[i])
 			testY = np.append(testY,y[i])
-		elif i%4!=2 :
+		elif i%4!=2 :#3/4 for train
 			for j in range(degree,0,-1):
 				row = np.append(row,math.pow(x[i],j))
 			row = np.append(row,1)
 			nA = np.append(nA,row)
 			plotX = np.append(plotX,x[i])
 			trainY = np.append(trainY,y[i])
+			
 	result = Reression(nA,plotX,testX,testY,trainX,trainY,degree,trainSize,testSize)
 	print('train Error ',result[1])
 	print('test Error	',result[0])
 	if degree == 1:
-		plt.scatter(plotX,trainY,s=40,c='blue',marker='o',alpha=0.5,label='train(15)')
-		plt.scatter(testX,testY,s=40,c='green',marker='x',alpha=0.5,label='test(5)')
-		plt.plot(plotX,result[3],color='r',label='1-degree')
+		plt.scatter(plotX,trainY,s=40,c='blue',marker='o',alpha=0.5,label='train')
+		plt.scatter(testX,testY,s=40,c='green',marker='x',alpha=0.5,label='test')
+		plt.plot(plotX,result[3],color='r')
 	elif degree == 5:
-		plt.plot(plotX,result[3],color='g',label='5-degree')
+		plt.plot(plotX,result[3],color='g')
 	elif degree ==10:
-		plt.plot(plotX,result[3],color='y',label='10-degree')
+		plt.plot(plotX,result[3],color='y')
 	elif degree ==14:	
-		plt.plot(plotX,result[3],color='pink',label='14-degree')
+		plt.plot(plotX,result[3],color='pink')
--- hw1.py
+++ hw1.py
@@ -10,59 +10,168 @@ noise=np.random.normal(0,1,20)
 #noise = rng.randn(20)
 x = np.linspace(3,-3,20)
 y=2*x+noise
-plt.subplot(2,1,1)
+plt.subplot(3,2,1)
 plt.plot([0,1],[0,1])
+plt.title('dataSize = 20',fontsize=10)
 
 
-
-print('degree1 result:')
-fun.linearRegression(x,y,1,20)
+print('------degree:1  dataSize:20  result:------')
+fun.linearRegression(x,y,1,20)#(x,y,degree,dataSize)
 fun.LeaveOneOut(x,y,1,20)
 fun.Five_Fold(x,y,1,20)
-print('------------')
 
-print('degree5 result:')
+print('------degree:5 dataSize:20 result:-------')
 fun.linearRegression(x,y,5,20)
 fun.LeaveOneOut(x,y,5,20)
 fun.Five_Fold(x,y,5,20)
-print('------------')
 
-print('degree10 result:')
+print('------degree:10 dataSize:20  result:------')
 fun.linearRegression(x,y,10,20)
 fun.LeaveOneOut(x,y,10,20)
 fun.Five_Fold(x,y,10,20)
-print('------------')
 
-print('degree14 result:')
+print('------degree:14 dataSize:20  result:------')
 fun.linearRegression(x,y,14,20)
 fun.LeaveOneOut(x,y,14,20)
 fun.Five_Fold(x,y,14,20)
-print('------------')
 
 plt.xlabel('x軸',fontsize=8)
 plt.ylabel('y軸',fontsize=8,rotation=0)
-plt.savefig('data_point')
-plt.legend(loc='upper left')
+plt.legend(loc='best')
+print('')
+print('')
+########c########
 
-########c.d########
+print("------partC model------")
+plt.subplot(3,2,2)
+plt.title('dataSize = 20,sin function',fontsize='10')
 
-plt.subplot(2,1,2)
 np.random.seed(500)
-noise2=np.random.normal(0,0.4,100)
-sinX = np.linspace(3,-3,100)#
+noise2=np.random.normal(0,0.4,20)
+sinX = np.linspace(3,-3,20)#
 sinY = np.sin(sinX * math.pi*2)
 sinYn = np.sin(sinX*math.pi*2) + noise2
 
-plt.plot(sinX,sinY,color='r',label="fff")
-plt.plot(sinX,sinYn,color='blue',label="fff")
+plt.plot(sinX,sinY,color='r',label="fff") #origin
+plt.plot(sinX,sinYn,color='blue',label="fff")#noise function
+
+print("degree:1 dataSize:20")
+fun.linearRegression(sinX,sinY,1,20)
+print("degree:5 dataSize:20")
+fun.linearRegression(sinX,sinY,5,20)
+print("degree:10 dataSize:20")
+fun.linearRegression(sinX,sinY,10,20)
+print("degree:14 dataSize:20")
+fun.linearRegression(sinX,sinY,14,20)
 
 
-fun.linearRegression(sinX,sinY,1,100)
-fun.linearRegression(sinX,sinY,5,100)
-fun.linearRegression(sinX,sinY,10,100)
-fun.linearRegression(sinX,sinY,14,100)
 
+print('')
+print('')
+
+
+#######d-60######
+x = np.linspace(3,-3,60)
+noise=np.random.normal(0,1,60)
+y=2*x+noise
+plt.subplot(3,2,3)
+plt.title('dataSize = 60',fontsize=10)
+
+print('------degree:1 dataSize:60 result:------')
+fun.linearRegression(x,y,1,60)#(x,y,degree,dataSize)
+fun.LeaveOneOut(x,y,1,60)
+fun.Five_Fold(x,y,1,60)
+
+print('------degree:5 dataSize:60 result:------')
+fun.linearRegression(x,y,5,60)
+fun.LeaveOneOut(x,y,5,60)
+fun.Five_Fold(x,y,5,60)
+
+print('------degree:10 dataSize:60 result:------')
+fun.linearRegression(x,y,10,60)
+fun.LeaveOneOut(x,y,10,60)
+fun.Five_Fold(x,y,10,60)
+
+print('------degree:14 dataSize:60 result:------')
+fun.linearRegression(x,y,14,60)
+fun.LeaveOneOut(x,y,14,60)
+fun.Five_Fold(x,y,14,60)
+
+print('')
+print('')
+
+plt.xlabel('x軸',fontsize=8)
+plt.ylabel('y軸',fontsize=8,rotation=0)
+plt.legend(loc='best')
+
+#######d-160######
+x = np.linspace(3,-3,160)
+noise=np.random.normal(0,1,160)
+y=2*x+noise
+plt.subplot(3,2,4)
+plt.title('dataSize = 160',fontsize=10)
+
+print('------degree:1 dataSize:160 result:------')
+fun.linearRegression(x,y,1,160)#(x,y,degree,dataSize)
+fun.LeaveOneOut(x,y,1,160)
+fun.Five_Fold(x,y,1,160)
+
+print('------degree:5 dataSize:160 result:------')
+fun.linearRegression(x,y,5,160)
+fun.LeaveOneOut(x,y,5,160)
+fun.Five_Fold(x,y,5,160)
+
+print('------degree:10 dataSize:160 result:------')
+fun.linearRegression(x,y,10,160)
+fun.LeaveOneOut(x,y,10,160)
+fun.Five_Fold(x,y,10,160)
+
+print('------degree:14 dataSize:160 result:------')
+fun.linearRegression(x,y,14,160)
+fun.LeaveOneOut(x,y,14,160)
+fun.Five_Fold(x,y,14,160)
+
+print('')
+print('')
+
+plt.xlabel('x軸',fontsize=8)
+plt.ylabel('y軸',fontsize=8,rotation=0)
+plt.legend(loc='best')
+
+#######d-320######
+x = np.linspace(3,-3,320)
+noise=np.random.normal(0,1,320)
+y=2*x+noise
+plt.subplot(3,2,5)
+plt.title('dataSize = 320',fontsize=10)
+
+print('------degree:1 dataSize:320 result:------')
+fun.linearRegression(x,y,1,320)#(x,y,degree,dataSize)
+fun.LeaveOneOut(x,y,1,320)
+fun.Five_Fold(x,y,1,320)
+
+print('------degree:5 dataSize:320 result:------')
+fun.linearRegression(x,y,5,320)
+fun.LeaveOneOut(x,y,5,320)
+fun.Five_Fold(x,y,5,320)
+
+print('------degree:10 dataSize:320 result:------')
+fun.linearRegression(x,y,10,320)
+fun.LeaveOneOut(x,y,10,320)
+fun.Five_Fold(x,y,10,20)
+
+print('------degree:14 dataSize:320 result:------')
+fun.linearRegression(x,y,14,320)
+fun.LeaveOneOut(x,y,14,320)
+fun.Five_Fold(x,y,14,320)
+
+print('')
+print('')
+
+plt.xlabel('x軸',fontsize=8)
+plt.ylabel('y軸',fontsize=8,rotation=0)
+plt.legend(loc='best')
+
+
+plt.savefig('data_point')
 plt.show()
-
-
-
